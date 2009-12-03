3f7f326f27728f366963b3144ddde5d62cb4ad64
Yann Klis <yann.klis@novelys.com>
2009-12-03 15:03:45 +0100

    Register users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :user_session
   map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
 
+  map.resource :account, :controller => "users"
+  map.resources :users
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
